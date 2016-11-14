f6820abf10c603ccec3c7f85a9f492cdf43e42e9
hotryo123 <hotryo123@yahoo.co.jp>
2016-11-14 14:29:27 +0900

    updated

--- WC_CheckTickerHistory.sql
+++ WC_CheckTickerHistory.sql
@@ -56,6 +56,63 @@ order by
 	l.DsLocalCode
 
 
+/*--------------------
+ 2. All US stocks
+ 
+ About Google:	Google is listed on NASDAQ as GOOG and GOOGL. GOO"G" owner doesn't have a voting right while "L" does.
+				This split has occured on April 2014. The reason why they split it is to protect owners' major votes.
+				"G" is called as "Class C". "L" is the class B that grants 1 vote with 10 shares. The owners have class A grating 1 vote for 1 share.
+
+				In the next year of class split, Google founded Alphabet which is a parent company of Google, Google Ventures etc..
+				The tickers have not changed from the originals (GOOG (C) and GOOGL(B))
+
+---------------------*/
+drop table #wsus
+select
+    m.*
+,   p.*
+/*
+,   p.isrcode  -- Issuer code
+,   p.code  -- SecCode to map with the master table
+,   p.typ_      -- Security type where 1=North America, 6=Global
+,   p.vencode */
+into #wsus
+from vw_securityMasterX m
+    join vw_wsCompanyMapping p on p.code = m.seccode and p.type_ = m.typ
+where 
+    m.Country='USA'
+    and p.type_ = 1
+
+select * from #wsus order by name
+
+-- With ticker (from DS2)
+drop table #ds2us
+select
+	dmap.*
+,	difo.*
+into #ds2us
+from
+	vw_Ds2Mapping dmap
+	join vw_Ds2SecInfo difo on dmap.VenCode=difo.InfoCode
+where
+	difo.IsPrimQt = 1
+	and difo.CountryTradingInName='UNITED STATES'
+
+select * from #ds2us order by dsqtname -- ALPHABET B (GOOGL) doesn't exist in DS2
+
+select
+	w.*
+,	d.*
+,	l.DsLocalCode
+from
+	#wsus w
+	join #ds2us d on w.seccode=d.seccode
+	join Ds2CtryQtInfo l on d.InfoCode=l.InfoCode
+order by
+	d.dsqtname
+
+
+
 --------------------------------------------------------
 -- 2. Data items
 --------------------------------------------------------
@@ -116,61 +173,3 @@ where
 -- Bessi @SUMIT40
 -- 
 
-
-------------------------------------------------------------------------
--- 3. Summarize and format the table and delete unnecessary temp tables
-------------------------------------------------------------------------
-select
-	mname.*
-from
-	#measlist mlst
-	join TRECode mname on mlst.Measure=mname.Code
-where
-	mname.CodeType=5
-
-drop table #allg_ibes2
-drop table #ds2all
-
--------------------------------------
--- 4. Sample KPI data for each stock 
--------------------------------------
-select * from #ibsdsall
-
-select
-	tsum.EstPermID
-,	tsum.Measure
-,	mcod.Description
-,	Min(tsum.PerEndDate) as 'PerEndDate'
-,	tsum.DefMeanEst
-from
-	TRESumPer tsum
-	join TRECode mcod on tsum.Measure = mcod.Code and mcod.CodeType=5
-where
-	tsum.EstPermID=30064783263
-	and tsum.ExpireDate is null
-	and tsum.PerEndDate > GetDate()
-group by
-	tsum.EstPermID, tsum.Measure, mcod.Description, tsum.DefMeanEst
-
-	-- and IsParent=0 -- Consolidated
-	-- and PerType=4 -- Year
-	-- and (DateDiff(year, PerEndDate, EffectiveDate) = 0 -- temporary specify Konki
-	-- or (EffectiveDate>='2016-07-25' and DateDiff(year, PerEndDate, EffectiveDate) <0))
-
-
-
-select
-        tsum.EstPermID
-,       tsum.Measure
-,       mcod.Description
-,       Min(tsum.PerEndDate) as 'PerEndDate'
-,       tsum.DefMeanEst
-from
-        TRESumPer tsum
-        join TRECode mcod on tsum.Measure = mcod.Code and mcod.CodeType=5
-where
-        tsum.EstPermID= 30064795966
-        and tsum.ExpireDate is null
-        and tsum.PerEndDate > GetDate()
-group by
-        tsum.EstPermID, tsum.Measure, mcod.Description, tsum.DefMeanEst
